6525e2741b9184656056610360016e4e669d6abe
Levi Yoder Raskin <122696329+Levi-Raskin@users.noreply.github.com>
2025-07-05 17:08:17 -0700

    commits

--- scripts/Figures.R
+++ scripts/Figures.R
@@ -358,6 +358,225 @@ ggsave(filename = paste(output, "3Dfig1LDDMMAlpha08.svg", sep = ""),
        width = 3,
        height = 45)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+lddmmRes1Alpha0.2 <- read.delim("/Users/levir/Documents/GitHub/PCAPhylogenetics/results/Mongle_et_al_2023_RB/SimRes/LDDMMSimRes/threeDimensionSimTreeIndex0LM25Alpha0.200000Dataset0nodeShapes.tsv", header = FALSE)
+colnames(lddmmRes1Alpha0.2) <- c("taxon", "id", "x", "y", "z")
+lddmmRes1Alpha0.2$taxon <- gsub("_", lddmmRes1Alpha0.2$taxon, replacement = " ")
+
+tip_order <- p1$data %>%
+  filter(isTip) %>%
+  arrange(-y) %>%
+  pull(label)
+
+
+# Make individual plots with custom y range
+compute_global_xrange <- function(data, taxa) {
+  all_proj_x <- c()
+  
+  for (tip in taxa) {
+    dat <- dplyr::filter(data, taxon == tip)
+    dat$x <- dat$x - mean(dat$x)
+    dat$y <- dat$y - mean(dat$y)
+    dat$z <- dat$z - mean(dat$z)
+    
+    x2d <- dat$x - dat$y * 0.5  # same as in your project_points()
+    all_proj_x <- c(all_proj_x, x2d)
+  }
+  
+  return(range(all_proj_x))
+}
+stackFunc <- function(tip) {
+  dat <- filter(lddmmRes1Alpha0.2, taxon == tip)
+  
+  #center dat
+  dat$x <- dat$x - mean(dat$x)
+  dat$y <- dat$y - mean(dat$y)
+  dat$z <- dat$z - mean(dat$z)
+  
+  y_pad <- 0.6  # Add vertical padding
+  y_min <- min(dat$y) - y_pad
+  y_max <- max(dat$y) + y_pad
+  
+  df <- dat[,3:5]
+  
+  hull_faces <- geometry::convhulln(df, output.options = TRUE)$hull
+  
+  get_edges <- function(faces) {
+    edges <- do.call(rbind, lapply(1:nrow(faces), function(i) {
+      tri <- faces[i, ]
+      rbind(tri[c(1, 2)], tri[c(2, 3)], tri[c(3, 1)])
+    }))
+    edges <- unique(t(apply(edges, 1, sort)))
+    return(edges)
+  }
+  edges <- get_edges(hull_faces)
+  hull_vertices <- unique(as.vector(hull_faces))
+  
+  project_points <- function(x, y, z) {
+    x2d <- x - y * 0.5
+    y2d <- z - y * 0.5
+    return(data.frame(x2d = x2d, y2d = y2d))
+  }
+  
+  projected_df <- project_points(df$x, df$y, df$z)
+  
+  edge_df <- do.call(rbind, lapply(1:nrow(edges), function(i) {
+    idx1 <- edges[i, 1]
+    idx2 <- edges[i, 2]
+    
+    p1 <- df[idx1, ]
+    p2 <- df[idx2, ]
+    
+    proj1 <- projected_df[idx1, ]
+    proj2 <- projected_df[idx2, ]
+    
+    mean_z <- mean(c(p1$z, p2$z))
+    
+    data.frame(
+      x = proj1$x2d, y = proj1$y2d,
+      xend = proj2$x2d, yend = proj2$y2d,
+      depth = mean_z
+    )
+  }))
+  
+  depth_range <- range(edge_df$depth)
+  edge_df$gray <- (edge_df$depth - depth_range[1]) / diff(depth_range)
+  edge_df$color <- gray(1 - edge_df$gray)
+  
+  hull_points <- df[hull_vertices, ]
+  projected_points <- project_points(hull_points$x, hull_points$y, hull_points$z)
+  projected_points$z <- hull_points$z
+  projected_points$gray <- (projected_points$z - min(df$z)) / diff(range(df$z))
+  projected_points$color <- gray(1 - projected_points$gray)
+  
+  projected_points <- projected_points %>%
+    arrange(z)
+  ggplot() +
+    geom_segment(data = edge_df,
+                 aes(x = x, 
+                     y = y, 
+                     xend = xend, 
+                     yend = yend, 
+                     color = depth,
+                     alpha = depth),
+                 linewidth = 1) +
+    scale_alpha_continuous(range = c(0.4, 0.6))+
+    geom_point(data = projected_points,
+               aes(x = x2d, 
+                   y = y2d, 
+                   color = z, 
+                   size = z)) +
+    scale_color_gradient(low = "grey", high = "black") +
+    coord_cartesian(xlim = x_range, ylim = c(y_min, y_max)) +
+    theme_minimal() +
+    ggtitle(tip) +
+    theme(
+      plot.title = element_text(hjust = 0, size = 9),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid = element_blank(),
+      legend.position="none",
+      plot.margin = margin(2, 2, 2, 2)
+    )
+}
+
+taxa <- unique(lddmmRes1Alpha0.2$taxon)
+x_range <- compute_global_xrange(lddmmRes1Alpha0.2, taxa)
+plots <- lapply(tip_order, stackFunc)
+
+# Stack vertically
+stack_plot <-patchwork::wrap_plots(plots, ncol = 1)
+stack_plot
+ggsave(filename = paste(output, "3Dfig1LDDMMAlpha02LM25.svg", sep = ""), 
+       plot = stack_plot, 
+       width = 3,
+       height = 45)
+
+
+
+lddmmRes1Alpha0.2 <- read.delim("/Users/levir/Documents/GitHub/PCAPhylogenetics/results/Mongle_et_al_2023_RB/SimRes/LDDMMSimRes/threeDimensionSimTreeIndex0LM25Alpha0.400000Dataset0nodeShapes.tsv", header = FALSE)
+colnames(lddmmRes1Alpha0.2) <- c("taxon", "id", "x", "y", "z")
+lddmmRes1Alpha0.2$taxon <- gsub("_", lddmmRes1Alpha0.2$taxon, replacement = " ")
+
+tip_order <- p1$data %>%
+  filter(isTip) %>%
+  arrange(-y) %>%
+  pull(label)
+
+taxa <- unique(lddmmRes1Alpha0.2$taxon)
+x_range <- compute_global_xrange(lddmmRes1Alpha0.2, taxa)
+plots <- lapply(tip_order, stackFunc)
+
+# Stack vertically
+stack_plot <-patchwork::wrap_plots(plots, ncol = 1)
+stack_plot
+ggsave(filename = paste(output, "3Dfig1LDDMMAlpha04LM25.svg", sep = ""), 
+       plot = stack_plot, 
+       width = 3,
+       height = 45)
+
+
+
+lddmmRes1Alpha0.2 <- read.delim("/Users/levir/Documents/GitHub/PCAPhylogenetics/results/Mongle_et_al_2023_RB/SimRes/LDDMMSimRes/threeDimensionSimTreeIndex0LM25Alpha0.600000Dataset0nodeShapes.tsv", header = FALSE)
+colnames(lddmmRes1Alpha0.2) <- c("taxon", "id", "x", "y", "z")
+lddmmRes1Alpha0.2$taxon <- gsub("_", lddmmRes1Alpha0.2$taxon, replacement = " ")
+
+tip_order <- p1$data %>%
+  filter(isTip) %>%
+  arrange(-y) %>%
+  pull(label)
+
+taxa <- unique(lddmmRes1Alpha0.2$taxon)
+x_range <- compute_global_xrange(lddmmRes1Alpha0.2, taxa)
+plots <- lapply(tip_order, stackFunc)
+
+# Stack vertically
+stack_plot <-patchwork::wrap_plots(plots, ncol = 1)
+stack_plot
+ggsave(filename = paste(output, "3Dfig1LDDMMAlpha06LM25.svg", sep = ""), 
+       plot = stack_plot, 
+       width = 3,
+       height = 45)
+
+
+lddmmRes1Alpha0.2 <- read.delim("/Users/levir/Documents/GitHub/PCAPhylogenetics/results/Mongle_et_al_2023_RB/SimRes/LDDMMSimRes/threeDimensionSimTreeIndex0LM25Alpha0.800000Dataset0nodeShapes.tsv", header = FALSE)
+colnames(lddmmRes1Alpha0.2) <- c("taxon", "id", "x", "y", "z")
+lddmmRes1Alpha0.2$taxon <- gsub("_", lddmmRes1Alpha0.2$taxon, replacement = " ")
+
+tip_order <- p1$data %>%
+  filter(isTip) %>%
+  arrange(-y) %>%
+  pull(label)
+
+taxa <- unique(lddmmRes1Alpha0.2$taxon)
+x_range <- compute_global_xrange(lddmmRes1Alpha0.2, taxa)
+plots <- lapply(tip_order, stackFunc)
+
+# Stack vertically
+stack_plot <-patchwork::wrap_plots(plots, ncol = 1)
+stack_plot
+ggsave(filename = paste(output, "3Dfig1LDDMMAlpha08LM25.svg", sep = ""), 
+       plot = stack_plot, 
+       width = 3,
+       height = 45)
+
+
 # Figure: BM continuous traits --------------------------------------------
 
 tree <- phyloList[[1]]
